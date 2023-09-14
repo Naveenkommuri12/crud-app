d4679885af68fe19a51f5b01f0f8252981e88c39
Naveenkommuri12 <143915194+Naveenkommuri12@users.noreply.github.com>
2023-09-14 16:52:16 -0400

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+// Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container d-flex justify-content-center">
+        <a className="navbar-brand" href="#">Employee Management System</a>
+        {/* Add navigation links or buttons here */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
